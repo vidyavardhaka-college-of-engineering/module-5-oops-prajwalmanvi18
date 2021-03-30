045a144a152a506556e09eda1baef1334295fde8
prajwalmanvi18 <prajwalmanvi86@gmail.com>
2021-03-30 11:50:34 +0000

    done

--- Program-7.cpp
+++ Program-7.cpp
@@ -11,10 +11,11 @@ int main()
     int n, fact = 1;
     std::cout << "Enter a positive integer: ";
     std::cin >> n;
-   // for(int i = 1; i <=n; ++i)
-    //{
+    for(int i = 1; i <=n; ++i)
+    {
+      fact=fact*i;
         //Fill the missing statement
-    //}
+    }
     cout <<fact;    
     return 0;
 }
