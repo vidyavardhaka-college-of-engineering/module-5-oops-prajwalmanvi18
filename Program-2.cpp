a52ccb221f7741300a42aadad3ac87cb061c28b9
prajwalmanvi18 <prajwalmanvi86@gmail.com>
2021-03-30 11:38:31 +0000

    done

--- Program-2.cpp
+++ Program-2.cpp
@@ -8,9 +8,10 @@ using namespace std;
 int main() 
 {
    
-   int ; // Declare Variables
+   int l,b,area ; // Declare Variables
    std::cout << "\nEnter the Length and Breadth of Rectangle : ";
    std::cin >> l >> b;
+   area=l*b;
               //complete the missing code to caluculate area
    std::cout << area;
    return 0;
