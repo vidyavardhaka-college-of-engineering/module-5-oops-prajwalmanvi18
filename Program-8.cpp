084f9227b261bf97f5efb9219e896a5486c637f3
prajwalmanvi18 <prajwalmanvi86@gmail.com>
2021-03-30 11:51:55 +0000

    done

--- Program-8.cpp
+++ Program-8.cpp
@@ -8,13 +8,15 @@
 using namespace std;
 int main()
 {
-    int n, sum = 0, ; //complete the declaration part
+    int n, sum = 0, i=0; //complete the declaration part
 
     std::cout << "Enter a positive integer: ";
     std::cin >> n;
 
     while(i<=n)
     {
+      sum=sum+i;
+      i++;
         //complete the missing code
         //complete the missing code
     }
